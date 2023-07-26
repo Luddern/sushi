0c07130034907d995f98a154d2a56ed8835edf6f
Ludde <t66220092@gmail.com>
2023-07-26 17:45:33 +0800

    修改登入頁面提醒 footerBox修整

--- js/index.js
+++ js/index.js
@@ -16,7 +16,18 @@ $(document).ready(function () {
     }
   };
 });
+function showWelcomeMessage(event) {
+  event.preventDefault(); // 取消表單預設的提交行為
 
+  const username = document.getElementsByName("username")[0].value; // 取得使用者名稱
+  const welcomeMessage = `歡迎回來，${username}!`; // 歡迎訊息文字
+
+  // 使用彈跳視窗顯示歡迎訊息
+  alert(welcomeMessage);
+
+  // 隱藏登入視窗（這裡假設你的登入視窗的id為'id01'）
+  document.getElementById("id01").style.display = "none";
+}
 function login() {
   $("#id01").css("display", "block");
   console.log("123");
